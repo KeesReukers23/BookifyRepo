554a2a930f90c54bbccd96cf5ec6e52e9b7ba5f4
KeesReukers23 <k.reukers@student.fontys.nl>
2024-11-11 15:22:02 +0100

    git

--- backend/bookify/obj/Debug/net8.0/bookifyWEBApi.AssemblyInfo.cs
+++ backend/bookify/obj/Debug/net8.0/bookifyWEBApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bookifyWEBApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3ef64d7748935b29e742ba5124789578daf89d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ad90fbdbd82881c748704ffbf4f70b63d0ab3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("bookifyWEBApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bookifyWEBApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
